dd3979fc80295623c4c95a9062716b0547e7939a
Rodrigocrypt <73792484+Rodrigocrypt@users.noreply.github.com>
2020-11-22 12:37:41 +0000

    Update InformationGateway.sh

--- InformationGateway.sh
+++ InformationGateway.sh
@@ -1,2 +1,42 @@
 #!/bin/bash
 clear
+
+echo "********************************************************"
+echo "*****  POR QUESTOES DE SEGURANCA INSTALE O TOR    ******"
+echo "********************************************************"
+echo "********************************************************"
+echo "**** Script de Verificacao de sites Vulns a SQL ********"
+echo "********************************************************"
+echo "*****              Anon_X95                       ******"
+echo "********************************************************"
+echo ""
+read -p "Aperte para Prosseguir"
+
+clear
+
+while true
+do
+
+clear
+echo ""
+echo "******************************************************"
+echo "******         Selecione uma Opcao               *****"
+echo "******************************************************"
+echo "***** 1. Sqlmap (Menos Seguro)                   *****"
+echo "***** 2. Sqlmap + Tor (Mais Seguro)              *****"
+echo "***** 3. TESTE                                   *****"
+echo "******************************************************"
+echo "********             Anon_X95            *************"
+echo "******************************************************"
+read n
+echo ""
+case $n in
+
+1) clear
+echo "Informe a Url do Site:"
+read USITEX
+(xterm -title "SQLMAP" -e sqlmap -u $USITEX --dbs ) &
+
+esac
+done
+
